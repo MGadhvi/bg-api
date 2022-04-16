0baf5b3bf61eee63ec90f284d1a923ea3762ef75
MGD <mgd@posteo.net>
2022-04-16 23:36:09 +0100

    Initial Commit

---
+++ src/main/java/com/mg/bgapi/BgApiApplication.java
@@ -0,0 +1,13 @@
+package com.mg.bgapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BgApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mg/bgapi/BgApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mg.bgapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BgApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
